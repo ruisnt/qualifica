2b36f1492e67502aa809620bf4acd0116b0e4faa
Rui Santos <ruisntos@gmail.com>
2018-10-23 20:01:52 +0000

    Criação do projeto

---
+++ Qualifica.API/Data/QualificaAPIContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Qualifica.API.Models
+{
+    public class QualificaAPIContext : 
+    {
+        public QualificaAPIContext (DbContextOptions<QualificaAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Qualifica.API.Models.Usuario> Usuario { get; set; }
+    }
+}
